1cf17af5454e0aab197877a85c2588991476a047
Mukul Bisen <bthemukul@users.noreply.github.com>
2017-02-08 17:03:39 +0530

    Update 3-Data Structure.js

--- 3-Data Structure.js
+++ 3-Data Structure.js
@@ -141,3 +141,11 @@ function deepEqual(x,y){
   return keyinx==keyiny ;
   
 }
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
